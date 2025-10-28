99470e2480347272dc96a58e469c751d7018bff6
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2025-10-28 23:02:52 +0900

    refactor: 가독성을 위해 자연수 검증 함수 삭제

--- src/main/java/lotto/service/LottoGenerator.java
+++ src/main/java/lotto/service/LottoGenerator.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class LottoGenerator {
 
     public List<Lotto> create(int count) {
-        if(!isNaturalNum(count)){
+        if (count < 1) {
             throw new IllegalArgumentException("[ERROR] 0이상의 숫자만 입력받을 수 있습니다.");
         }
 
@@ -18,12 +18,6 @@ public class LottoGenerator {
             lottos.add(create());
         }
         return lottos;
-    }
-
-    private boolean isNaturalNum(int count) {
-        if (count < 1)
-            return false;
-        return true;
     }
 
     private Lotto create() {
