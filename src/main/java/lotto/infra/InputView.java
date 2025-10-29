cc3b638e496a7a83e6df156857f514f4dae7d5ac
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2025-10-29 23:30:07 +0900

    feat(infra): 사용자 입력값 받기

---
+++ src/main/java/lotto/infra/InputView.java
@@ -0,0 +1,48 @@
+package lotto.infra;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class InputView {
+
+    private static final String MONEY_REQUEST = "구입금액을 입력해 주세요.";
+    private static final String WIN_NUMBERS_REQUEST = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_REQUEST = "보너스 번호를 입력해 주세요.";
+    private static final String INPUT_ERROR = "잘못된 입력 형식입니다.";
+
+    private String input() {
+        return Console.readLine();
+    }
+
+    public int inputMoney() {
+        try {
+            System.out.println(MONEY_REQUEST);
+            return Integer.parseInt(input());
+        } catch (NumberFormatException ex) {
+            throw new IllegalArgumentException(INPUT_ERROR);
+        }
+    }
+
+    public List<Integer> inputWinNumbers() {
+        System.out.println(WIN_NUMBERS_REQUEST);
+        String input = input();
+
+        if (!input.matches("\\d{1,2}(,\\d{1,2})*")) {
+            throw new IllegalArgumentException(INPUT_ERROR);
+        }
+        return Arrays.stream(input.split(","))
+                .map(Integer::parseInt)
+                .toList();
+    }
+
+    public int inputBonus() {
+        try {
+            System.out.println(BONUS_REQUEST);
+            return Integer.parseInt(input());
+        } catch (NumberFormatException ex) {
+            throw new IllegalArgumentException(INPUT_ERROR);
+        }
+    }
+}
