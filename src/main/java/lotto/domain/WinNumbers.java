420292aaef8bb6434e6672f538b482916877a5b8
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2025-11-02 00:09:52 +0900

    feat(domain): 당첨 숫자 중복 검증 로직 추가
    에러 메시지 상수화하여 추가

--- src/main/java/lotto/domain/WinNumbers.java
+++ src/main/java/lotto/domain/WinNumbers.java
@@ -1,20 +1,30 @@
 package lotto.domain;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class WinNumbers {
 
     private final List<Integer> winNumber;
     private BonusNumber bonusNumber;
     private static final int NUMBER_SIZE = 6;
+    private static final String NUMBER_SIZE_ERROR ="당첨 숫자는 6개 입니다.";
+    private static final String NUMBER_RANGE_ERROR ="로또 숫자는 1-45사이의 숫자입니다.";
+    private static final String NUMBER_DUPLICATE_ERROR ="로또 숫자는 중복될 수 없습니다.";
 
     public WinNumbers(List<Integer> winNumber) {
-        if(validateNumberSize(winNumber, NUMBER_SIZE)){
-            throw new IllegalArgumentException();
+        if(!validateNumberSize(winNumber, NUMBER_SIZE)){
+            throw new IllegalArgumentException(NUMBER_SIZE_ERROR);
         }
 
-        if(validateAllRanges(winNumber)){
-         throw new IllegalArgumentException();
+        if(!validateAllRanges(winNumber)){
+         throw new IllegalArgumentException(NUMBER_RANGE_ERROR);
+        }
+
+        if(!validateDuplicateNumber(winNumber)){
+            throw new IllegalArgumentException(NUMBER_DUPLICATE_ERROR);
         }
 
         this.winNumber = winNumber;
@@ -39,6 +49,14 @@ public class WinNumbers {
 
     private boolean validateNumberSize(List numberss, int size) {
         if (numberss.size() != size) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateDuplicateNumber(List<Integer> winNumber){
+        Set<Integer> numberSet = new HashSet<>(winNumber);
+        if(numberSet.size() != winNumber.size()){
             return false;
         }
         return true;
