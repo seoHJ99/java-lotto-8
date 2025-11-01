84b44534a161c8b97721ebf509854e56c332a4b4
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2025-11-01 23:51:46 +0900

    refactor(view): 입력값 받는 클래스는 싱글톤으로 변경
    신규 객체가 생성될 필요가 없다고 판단. 메모리 관리를 위해 싱글톤으로 변경

--- src/main/java/lotto/infra/InputView.java
+++ src/main/java/lotto/infra/InputView.java
@@ -4,13 +4,25 @@ import camp.nextstep.edu.missionutils.Console;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
-public class InputView {
+public class InputView { // 싱글톤으로 교체 예정
 
     private static final String MONEY_REQUEST = "구입금액을 입력해 주세요.";
     private static final String WIN_NUMBERS_REQUEST = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_REQUEST = "보너스 번호를 입력해 주세요.";
     private static final String INPUT_ERROR = "잘못된 입력 형식입니다.";
+    private static InputView inputView;
+
+    private InputView() {
+    }
+
+    public static InputView getInstance() {
+        if (inputView == null) {
+            inputView = new InputView();
+        }
+        return inputView;
+    }
 
     private String input() {
         return Console.readLine();
@@ -32,7 +44,8 @@ public class InputView {
         if (!input.matches("\\d{1,2}(,\\d{1,2})*")) {
             throw new IllegalArgumentException(INPUT_ERROR);
         }
-        return Arrays.stream(input.split(","))
+
+        return  Arrays.stream(input.split(","))
                 .map(Integer::parseInt)
                 .toList();
     }
