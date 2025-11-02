eddcdde276811a14cbba58c69565dd3ed64a3794
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2025-11-02 20:28:27 +0900

    feat(controller): 프로그램 실행.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,27 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.infra.InputView;
+import lotto.infra.OutputView;
+import lotto.service.LottoGenerator;
+import lotto.service.LottoResultService;
+import lotto.service.LottoSellerService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = InputView.getInstance();
+        OutputView outputView = OutputView.getInstance();
+
+        LottoSellerService lottoSellerService = new LottoSellerService(new LottoGenerator());
+        LottoResultService lottoResultService = new LottoResultService();
+
+        LottoController lottoController =
+                new LottoController(
+                        inputView,
+                        outputView,
+                        lottoSellerService,
+                        lottoResultService);
+
+        lottoController.run();
     }
 }
--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,3 +1,42 @@
+package lotto.controller;
+
+import lotto.domain.*;
+import lotto.infra.InputView;
+import lotto.infra.OutputView;
+import lotto.service.LottoResultService;
+import lotto.service.LottoSellerService;
+
+import java.util.List;
+import java.util.Map;
+
+public class LottoController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final LottoSellerService sellerService;
+    private final LottoResultService resultService;
+
+    public LottoController(InputView inputView, OutputView outputView, LottoSellerService sellerService, LottoResultService resultService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.sellerService = sellerService;
+        this.resultService = resultService;
+    }
+
+    public void run() {
+        BuyMoney money = inputMoney();
+        List<Lotto> lottos = sellerService.buyLotto(money);
+        outputView.outputAllLotto(lottos);
+
+        WinNumbers winNumbers = inputWinNum();
+        inputBonusNum(winNumbers);
+
+        Map<LottoRank, Integer> lottoRanks = resultService.getAllLottoResult(lottos, winNumbers);
+        double earningRate = resultService.getEarningRate(money, lottoRanks);
+        outputView.outputStatistics(lottoRanks);
+        outputView.outputTotalEarningRate(earningRate);
+    }
+
     private WinNumbers inputWinNum() {
         while (true) {
             try {
