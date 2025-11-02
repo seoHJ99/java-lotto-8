c08b04e295282932da2e0536f1457cfadcbed5ed
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2025-11-02 20:27:40 +0900

    refactor(view): 로또 결과 출력 기능 함수 분리

--- src/main/java/lotto/infra/OutputView.java
+++ src/main/java/lotto/infra/OutputView.java
@@ -3,8 +3,7 @@ package lotto.infra;
 import lotto.domain.Lotto;
 import lotto.domain.LottoRank;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class OutputView {
 
@@ -33,21 +32,32 @@ public class OutputView {
     }
 
     public void outputStatistics(Map<LottoRank, Integer> lottoRanks) {
+        List<LottoRank> ranks = new ArrayList<>(Arrays.asList(LottoRank.values()));
+        Collections.reverse(ranks);
+
         StringBuilder sb = new StringBuilder();
-        sb.append("당첨 통계%n");
-        sb.append("---%n");
-        for (LottoRank rank : LottoRank.values()) {
-            sb.append(String.format(
-                    "%d개 일치 (%d원) - %d개%n",
-                    rank.matchingCount,
-                    rank.prize,
-                    lottoRanks.getOrDefault(rank, 0)
-            ));
-        }
-        System.out.println(sb);
+        sb.append(String.format("당첨 통계%n"));
+        sb.append(String.format("---%n"));
+
+        System.out.println(makeLottoResult(sb, lottoRanks));
     }
 
-    public void outputTotalEarningRate(double earningRate){
+    public void outputTotalEarningRate(double earningRate) {
         System.out.println(String.format("총 수익률은 %.1f%%입니다.", earningRate));
+    }
+
+    private StringBuilder makeLottoResult(StringBuilder sb, Map<LottoRank, Integer> lottoRanks) {
+        List<LottoRank> ranks = new ArrayList<>(Arrays.asList(LottoRank.values()));
+        Collections.reverse(ranks);
+        for (LottoRank rank : ranks) {
+            if (rank == LottoRank.NONE) continue;
+
+            sb.append(String.format("%d개 일치", rank.matchingCount));
+            if (rank.bonusMatching) {
+                sb.append(", 보너스 볼 일치");
+            }
+            sb.append(String.format(" (%,d원) - %,d개%n", rank.prize, lottoRanks.getOrDefault(rank, 0)));
+        }
+        return sb;
     }
 }
