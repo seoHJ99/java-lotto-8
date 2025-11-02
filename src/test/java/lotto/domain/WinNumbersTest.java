3d9bd04f26772edc1146e1095b919380c1563fde
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2025-11-02 20:31:35 +0900

    test: 도메인 생성 테스트 추가. 로또 판매 테스트 수정

---
+++ src/test/java/lotto/domain/WinNumbersTest.java
@@ -0,0 +1,33 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class WinNumbersTest {
+
+    @Test
+    void 당첨번호는_숫자_6개가_아니면_예외가_발생한다(){
+        assertThatThrownBy(() -> new WinNumbers(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 당첨번호에_중복_숫자가_있으면_예외가_발생한다() {
+        List<Integer> winNumbers = List.of(1, 2, 3, 4, 6, 6);
+        assertThatThrownBy(() -> new WinNumbers(winNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1, 47})
+    void 당첨번호_숫자는_(int num) {
+        List<Integer> winNumbers = List.of(1, 2, 3, 4, 5, num);
+        assertThatThrownBy(() -> new WinNumbers(winNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
--- src/test/java/lotto/service/LottoSellerServiceTest.java
+++ src/test/java/lotto/service/LottoSellerServiceTest.java
@@ -1,5 +1,6 @@
 package lotto.service;
 
+import lotto.domain.BuyMoney;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -17,18 +18,19 @@ class LottoSellerServiceTest {
         lottoSellerService = new LottoSellerService(new LottoGenerator());
     }
 
-    @Test
-    void 로또_판매_기능_테스트() {
-        assertThat(lottoSellerService.buyLotto(3000).size()).isEqualTo(3);
-        assertThat(lottoSellerService.buyLotto(10000).size()).isEqualTo(10);
+    @ParameterizedTest
+    @ValueSource(ints = {3000, 10000})
+    void 로또_판매_기능_테스트(int money) {
+        BuyMoney buyMoney = new BuyMoney(money);
+        assertThat(lottoSellerService.buyLotto(buyMoney).size()).isEqualTo(3);
+        assertThat(lottoSellerService.buyLotto(buyMoney).size()).isEqualTo(10);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {11000, 200})
-    void 로또_판매_예외_테스트() {
-        assertThatThrownBy(() -> lottoSellerService.buyLotto(1100))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR]");
+    void 로또_판매_예외_테스트(int money) {
+        BuyMoney buyMoney = new BuyMoney(money);
+        assertThatThrownBy(() -> lottoSellerService.buyLotto(buyMoney))
+                .isInstanceOf(IllegalArgumentException.class);
     }
-
 }
